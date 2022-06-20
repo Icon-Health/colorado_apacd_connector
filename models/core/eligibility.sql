00215b6b8459ca6f850c5a763497a76372d901dd
Mtully@iconhealthco.com <Mtully@iconhealthco.com>
2022-06-20 14:07:05 +0000

    fixed multi-payer type

--- models/core/eligibility.sql
+++ models/core/eligibility.sql
@@ -1,5 +1,5 @@
 select 
-	cast(e.member_id as String) as patient_id
+	distinct cast(e.member_id as String) as patient_id
     ,cast(d.member_gender_cd as String) as gender
     ,cast((d.member_dob_year||"-01-01") as datetime) as birth_date
     ,cast(d.race_1_cd as String) as race
@@ -8,11 +8,13 @@ select
     ,null as deceased_flag
     ,cast(null as datetime) as death_date
     ,cast(d.payer_cd as String) as payer
-    ,cast(e.line_of_business_cd as String) as payer_type
+    ,cast(max(e.line_of_business_cd) as String) as payer_type
     ,cast(null as String) as dual_status
     ,cast(null as String) as medicare_status
-    ,cast(e.eligibility_month as int) as month
-    ,cast(extract(year from e.eligibility_dt) as int) as year
+    ,cast(min(e.eligibility_month) as int) as month
+    ,cast(min(extract(year from e.eligibility_dt)) as int) as year
 from {{ var('member_eligibility')}} e
-left join {{ var('member_demographics')}} d
+inner join {{ var('member_demographics')}} d
 	on e.member_id = d.member_id
+where d.member_id is not null
+group by patient_id, member_gender_cd, member_dob_year, race_1_cd, member_zip_cd, Member_state_cd, payer_cd
